57bc7fb5e62d637339f7d49d102e9a7fc81a7cdf
daniela <daniela.soucst@gmail.com>
2017-06-03 16:28:47 -0300

    atualizando...

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ TEMPO_DECOLAGEM = 2
 TEMPO_ABASTECIMENTO = 3
 TEMPO_DESEMBARQUE = 1
 
-A_INTER = 5 #Create a airplane every [min, max] seconds
-TEMPO_SIMULACAO = 120 #em segundos
+A_INTER = 4 #Create a airplane every a_inter minutes
+TEMPO_SIMULACAO = 60 #imulation time in minutes
 
 QTDE_PISTAS = 1
 QTDE_PONTES = 2
@@ -20,12 +20,19 @@ QTDE_TANQUES = 1
 PROB_ABASTECIMENTO = 0.5
 
 class Simulacao:
+    # Métricas de desempenho analisadas:
+    # número de aviões atendidos / hora;
+    # tempo médio por avião no solo
+    # utilização dos fingers e das pistas.
+
     def __init__(self):
         self.num_chegadas = 0  # self.num_arrivals = 0
         self.num_pousos = 0 # self.num_complet = 0
         self.num_abastecimentos = 0 # self.num_refuel = 0
+        self.num_desembarques = 0
         self.tempo_total_abastecimento = 0# self.num_refuel_time = 0
         self.num_decolagens = 0# self.num_departures = 0
+        self.tempo_total_solo = 0
 
     def new_chegada(self):
         self.num_chegadas += 1
@@ -34,11 +41,16 @@ class Simulacao:
         self.num_abastecimentos += 1
         self.tempo_total_abastecimento += time
 
-    def new_decolagem(self):
+    def new_decolagem(self,tempoEmSolo):
         self.num_decolagens += 1
+        self.tempo_total_solo += tempoEmSolo
+
 
     def new_pouso(self):
         self.num_pousos += 1
+
+    def new_desembarque(self):
+        self.num_desembarques += 1
 
     def report(self):
         print ('\n*** SimPy Simulation Report ***\n')
@@ -60,6 +72,13 @@ class Simulacao:
         u1 = s1 * x
         u2 = s2 * xr
         u3 = s3 * xd
+        #utilização das pontes
+        x_pontes = float(self.num_desembarques)/float(TEMPO_SIMULACAO)
+        u_pontes = float(x_pontes)*float(TEMPO_DESEMBARQUE)
+
+        #pistas
+        x_pistas = float(self.num_pousos+self.num_decolagens)/float(TEMPO_SIMULACAO)
+        u_pistas = float(x_pistas)*float(TEMPO_DESEMBARQUE+TEMPO_DESEMBARQUE)
 
         print ('Media de chegadas: %f' % a)
         print ('Media de saidas: %f' % d)
@@ -72,6 +91,14 @@ class Simulacao:
         print ('Utilizacao do sistema de chegadas: %f' % u1)
         print ('Utilizacao do sistema de reabastecimento: %f' % u2)
         print ('Utilizacao do sistema de saida: %f' % u3)
+
+
+
+        print("<<<<<NOssas métricas>>>>>>")
+        print ('Num. médio de aviões atendidos: %f' % d)
+        print ('Tempo médio por avião no solo: %f' % (self.tempo_total_solo/self.num_decolagens))
+        print('Throughput, utilização das pontes de desemb.: %2f,%2f'%(x_pontes,u_pontes))
+        print('Throughput, utilização das pistas.: %2f,%2f' % (x_pistas, u_pistas))
 
 class Aeroporto(object):
     def __init__(self, env,qtde_pistas,qtde_pontes,qtde_tanques,tempo_abastecimento,tempo_pouso,tempo_decolagem,tempo_desembarque):
@@ -109,11 +136,15 @@ def aviao(env, nome, aeroporto): #o aviao é um processo
     print('%s arrives at the airport at %.2f.' % (nome, env.now))
     simulacao.new_chegada()
     tempo_chegada = env.now
+    horario_pouso = 0
 
-    '''Simulando pedido de pouso, falta colocar requisição de ponte'''
+    '''Simulando pedido de pouso'''
+    # o with libera automaticamente o recurso depois q o processo termina seu uso, passando
+    # o uso assim para o proximo processo que está na fila
     with aeroporto.pistas.request() as request:
         yield request
         print('%s enters the airport at %.2f. Wait: %.2f' % (nome, env.now, env.now - tempo_chegada))
+        horario_pouso = env.now
         yield env.process(aeroporto.liberar_pouso(nome))
         simulacao.new_pouso()
 
@@ -121,7 +152,7 @@ def aviao(env, nome, aeroporto): #o aviao é um processo
         yield request
         print('%s requisita finger at %.2f. Wait: %.2f' % (nome, env.now, env.now - tempo_chegada))
         yield env.process(aeroporto.liberar_ponte(nome))
-        simulacao.new_pouso()
+        simulacao.new_desembarque()
 
     '''Simulando pedido de abastecimento'''
     sorte = random.randint(0, 100)
@@ -137,9 +168,10 @@ def aviao(env, nome, aeroporto): #o aviao é um processo
     with aeroporto.pistas.request() as request: #no caso, so temos uma pista
         yield request
         start = env.now
-        print('%s sai do airport at %.2f. Wait: %.2f' % (nome, env.now, env.now - tempo_chegada))
+        estadia = env.now - horario_pouso
+        print('%s sai do airport at %.2f. Wait: %.2f tempo em solo:%.2f ' % (nome, env.now, env.now - tempo_chegada,estadia))
         yield env.process(aeroporto.liberar_decolagem(nome))
-        simulacao.new_decolagem()
+        simulacao.new_decolagem(estadia)
 
     print('%s leaves the airport at %.2f. Service time: %.2f' % (nome, env.now, env.now - start))
 
@@ -153,7 +185,7 @@ def setup(env, qtde_pistas,qtde_pontes,qtde_tanques,tempo_abastecimento,tempo_po
 
     #Vamo botar mais avioes enquanto esta simulacao ocorre Create more cars while the simulation is running
     while True:
-        yield env.timeout(random.randint(a_inter-4, a_inter+4))
+        yield env.timeout(random.randint(a_inter-2, a_inter+2))
         i += 1
         env.process(aviao(env, 'Airplane %d' % i, aeroporto))
 
